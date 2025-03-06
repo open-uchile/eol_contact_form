e759cfb587803de949e3480f08bb3bef5f2cacec
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2025-03-06 18:07:07 -0300

    docs: update tag version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="eol_contact_form",
-    version="0.1.0",
+    version="0.2.0",
     author="Oficina EOL UChile",
     author_email="eol-ing@uchile.cl",
     description="Eol Contact Form",
